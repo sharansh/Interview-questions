b62952a2636dc0d4777bee2b616d3bff61fbd760
sharansh <sharansh@live.com>
2024-04-04 21:51:52 +0530

    Update tcsRandom.js

--- JS/Problem_Solving/tcsRandom.js
+++ JS/Problem_Solving/tcsRandom.js
@@ -22,3 +22,21 @@ function Inverse(obj){
 
 // var student = {name: 'jack', age: 12, std: 18, fees: 5000};
 //console.log(Inverse(student));
+
+var obj = {"1":5,"2":7,"3":0,"4":0,"5":0,"6":0,"7":0,"8":0,"9":0,"10":0,"11":0,"12":0}
+var result = Object.keys(obj).map((key) => [key, obj[key]]);
+
+console.log(result);
+
+const array = ["hello", "world", "this", "is", "an", "example"];
+
+const groupedArray = array.reduce((acc, current) => {
+  const key = current.length;
+  if (!acc[key]) {
+    acc[key] = [];
+  }
+  acc[key].push(current);
+  return acc;
+}, {});
+
+console.log(groupedArray);
